cc55cb7c8d2e828a9e5087145e6d87fd2983c436
macie <macie@DESKTOP-GUK2RJ2>
2021-04-13 23:12:28 +0200

    create DatabaseFirstSimpleExample project

---
+++ DatabaseFirstSimpleExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DatabaseFirstSimpleExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
