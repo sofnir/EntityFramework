bd29cf2db3cd953c9a6d4ba8da49de52c7d330d9
macie <macie@DESKTOP-GUK2RJ2>
2021-04-14 15:31:29 +0200

    add post if exist and return from db

--- DatabaseFirstSimpleExample/Program.cs
+++ DatabaseFirstSimpleExample/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DatabaseFirstSimpleExample.Models;
+using System;
+using System.Linq;
 
 namespace DatabaseFirstSimpleExample
 {
@@ -6,7 +8,31 @@ namespace DatabaseFirstSimpleExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var context = new DatabaseFirstSimpleExampleContext();
+
+            const int postId = 1;
+
+            var post = new Post()
+            {
+                PostId = postId,
+                DatePublished = DateTime.Now,
+                Title = "Title",
+                Body = "Body"
+            };
+
+            var postFromDb = context.Posts.SingleOrDefault(q => q.PostId == postId);
+
+            if (postFromDb == null)
+            {
+                context.Posts.Add(post);
+                context.SaveChanges();
+                postFromDb = context.Posts.Single(q => q.PostId == postId);
+            }
+
+            Console.WriteLine($"Id: {postFromDb.PostId}");
+            Console.WriteLine($"Date published: {postFromDb.DatePublished}");
+            Console.WriteLine($"Title: {postFromDb.Title}");
+            Console.WriteLine($"Body: {postFromDb.Body}");
         }
     }
 }
