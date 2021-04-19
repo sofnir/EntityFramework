4c0c77b77babf4a32ec74e51aa2aa295e29e3585
macie <macie@DESKTOP-GUK2RJ2>
2021-04-19 17:52:06 +0200

    create posts

---
+++ EntityFramework/Models/CodeFirstSimpleExampleDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DatabaseFirstSimpleExample.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstSimpleExample.Models
+{
+    public class CodeFirstSimpleExampleDbContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-GUK2RJ2;Database=CodeFirstSimpleExample;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ EntityFramework/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DatabaseFirstSimpleExample.Models
+{
+    public partial class Post
+    {
+        public int Id { get; set; }
+        public DateTime DatePublished { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
