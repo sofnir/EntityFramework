fc16afc55d29717f84d5f011558e84afd38b8dae
macie <macie@DESKTOP-GUK2RJ2>
2021-04-19 21:09:04 +0200

    add migrations to existing db first concept

---
+++ DatabaseFirst/Migrations/20210419190042_Init.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatabaseFirst.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CourseSections");
+
+            migrationBuilder.DropTable(
+                name: "CourseTags");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "tblUser");
+
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+        }
+    }
+}
