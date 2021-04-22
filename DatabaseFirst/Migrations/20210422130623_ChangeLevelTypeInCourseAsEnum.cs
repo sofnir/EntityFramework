8a699d1185526d78bf309b04dec0fc127e30ce93
macie <macie@DESKTOP-GUK2RJ2>
2021-04-22 15:09:20 +0200

    ChangeLevelTypeInCourseAsEnum

---
+++ DatabaseFirst/Migrations/20210422130623_ChangeLevelTypeInCourseAsEnum.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatabaseFirst.Migrations
+{
+    public partial class ChangeLevelTypeInCourseAsEnum : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Level",
+                table: "Courses",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(short),
+                oldType: "smallint");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<short>(
+                name: "Level",
+                table: "Courses",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
