e82e86ebdfd92c398ca27dd487b0202ee7fe1df1
macie <macie@DESKTOP-GUK2RJ2>
2021-04-23 16:45:26 +0200

    create models

---
+++ DataAnnotations/Models/PlutoContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst.Models
+{
+    public class PlutoContext : DbContext
+    {        
+        public PlutoContext()
+        {
+        }
+
+        public PlutoContext(DbContextOptions<PlutoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Tag> Tags { get; set; }        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-GUK2RJ2;Database=Pluto_DataAnnotations;Trusted_Connection=True;");
+            }
+        }
+    }
+}
