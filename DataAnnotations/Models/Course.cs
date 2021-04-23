6a5e2e56f5c3eb84dcbf70dfc668106cd32b0281
macie <macie@DESKTOP-GUK2RJ2>
2021-04-23 16:49:54 +0200

    AddAnnotationsToCourseTable

--- DataAnnotations/Models/Course.cs
+++ DataAnnotations/Models/Course.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +11,19 @@ namespace CodeFirst.Models
     public class Course
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
         public string Name { get; set; }
-        public string Description { get; set; }                
+
+        [Required]
+        [MaxLength(2000)]
+        public string Description { get; set; }
         public CourseLevel Level { get; set; }
         public float FullPrice { get; set; }
+
+        [ForeignKey("Author")]
+        public int AuthorId { get; set; }
         public Author Author { get; set; }
         public IList<Tag> Tags { get; set; }
 
