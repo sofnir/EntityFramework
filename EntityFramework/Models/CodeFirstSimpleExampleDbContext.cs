662428f07a8dd6d699405b8a5ec0013dd94e9b49
macie <macie@DESKTOP-GUK2RJ2>
2021-04-27 15:24:19 +0200

    rename model namespace in CodeFirstSimpleExample project

--- EntityFramework/Models/CodeFirstSimpleExampleDbContext.cs
+++ EntityFramework/Models/CodeFirstSimpleExampleDbContext.cs
@@ -1,5 +1,4 @@
-﻿using DatabaseFirstSimpleExample.Models;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- EntityFramework/Models/Post.cs
+++ EntityFramework/Models/Post.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 #nullable disable
 
-namespace DatabaseFirstSimpleExample.Models
+namespace CodeFirstSimpleExample.Models
 {
     public partial class Post
     {
