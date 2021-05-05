d05befa47776507179a46b7da1133877d5d76d39
macie <macie@DESKTOP-GUK2RJ2>
2021-05-05 12:06:41 +0200

    PopulateCourses in QueryingLINQ project

--- QueryingLINQ/Migrations/20210505100602_PopulateCourses.Designer.cs
+++ QueryingLINQ/Migrations/20210505100602_PopulateCourses.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QueryingLINQ.Models;
 
 namespace QueryingLINQ.Migrations
 {
     [DbContext(typeof(PlutoContext))]
-    partial class PlutoContextModelSnapshot : ModelSnapshot
+    [Migration("20210505100602_PopulateCourses")]
+    partial class PopulateCourses
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- QueryingLINQ/Models/PlutoContext.cs
+++ QueryingLINQ/Models/PlutoContext.cs
@@ -56,6 +56,89 @@ namespace QueryingLINQ.Models
                 new Author { Id = 5, Name = "John Smith" }
             );
 
+            modelBuilder.Entity<Course>().HasData(
+                new Course
+                {
+                    Id = 1, Name = "C# Basics",
+                    AuthorId = 1,
+                    FullPrice = 49,
+                    Description = "Description for C# Basics",
+                    Level = Course.CourseLevel.Beginner
+                },
+                new Course
+                {
+                    Id = 2,
+                    Name = "C# Intermediate",
+                    AuthorId = 1,
+                    FullPrice = 49,
+                    Description = "Description for C# Intermediate",
+                    Level = Course.CourseLevel.Intermediate
+                },
+                new Course
+                {
+                    Id = 3,
+                    Name = "C# Advanced",
+                    AuthorId = 1,
+                    FullPrice = 69,
+                    Description = "Description for C# Advanced",
+                    Level = Course.CourseLevel.Advanced
+                },
+                new Course
+                {
+                    Id = 4,
+                    Name = "Javascript: Understanding the Weird Parts",
+                    AuthorId = 2,
+                    FullPrice = 149,
+                    Description = "Description for Javascript",
+                    Level = Course.CourseLevel.Intermediate
+                },
+                new Course
+                {
+                    Id = 5,
+                    Name = "Learn and Understand AngularJS",
+                    AuthorId = 2,
+                    FullPrice = 99,
+                    Description = "Description for AngularJS",
+                    Level = Course.CourseLevel.Intermediate
+                },
+                new Course
+                {
+                    Id = 6,
+                    Name = "Learn and Understand NodeJS",
+                    AuthorId = 2,
+                    FullPrice = 149,
+                    Description = "Description for NodeJS",
+                    Level = Course.CourseLevel.Intermediate
+                },
+                new Course
+                {
+                    Id = 7,
+                    Name = "Programming for Complete Beginners",
+                    AuthorId = 3,
+                    FullPrice = 45,
+                    Description = "Description for Programming for Beginners",
+                    Level = Course.CourseLevel.Beginner
+                },
+                new Course
+                {
+                    Id = 8,
+                    Name = "A 16 Hour C# Course with Visual Studio 2013",
+                    AuthorId = 4,
+                    FullPrice = 150,
+                    Description = "Description 16 Hour Course",
+                    Level = Course.CourseLevel.Beginner
+                },
+                new Course
+                {
+                    Id = 9,
+                    Name = "Learn JavaScript Through Visual Studio 2013",
+                    AuthorId = 4,
+                    FullPrice = 20,
+                    Description = "Description Learn Javascript",
+                    Level = Course.CourseLevel.Beginner
+                }
+            );            
+
             base.OnModelCreating(modelBuilder);
         }
     }
