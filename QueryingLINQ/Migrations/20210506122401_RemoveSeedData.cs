1f6733243b2c26611bb450cbd8cc22c40170cbb1
macie <macie@DESKTOP-GUK2RJ2>
2021-05-06 14:24:39 +0200

    RemoveSeedData in QueryingLINQ proj

---
+++ QueryingLINQ/Migrations/20210506122401_RemoveSeedData.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QueryingLINQ.Migrations
+{
+    public partial class RemoveSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Mosh hamedani" },
+                    { 2, "Anthony Alicea" },
+                    { 3, "Eric Wise" },
+                    { 4, "Tom Owsiak" },
+                    { 5, "John Smith" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tags",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "c#" },
+                    { 2, "angularjs" },
+                    { 3, "javascript" },
+                    { 4, "nodejs" },
+                    { 5, "oop" },
+                    { 6, "linq" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Courses",
+                columns: new[] { "Id", "AuthorId", "CoverId", "Description", "FullPrice", "Level", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, 0, "Description for C# Basics", 49f, 1, "C# Basics" },
+                    { 2, 1, 0, "Description for C# Intermediate", 49f, 2, "C# Intermediate" },
+                    { 3, 1, 0, "Description for C# Advanced", 69f, 3, "C# Advanced" },
+                    { 4, 2, 0, "Description for Javascript", 149f, 2, "Javascript: Understanding the Weird Parts" },
+                    { 5, 2, 0, "Description for AngularJS", 99f, 2, "Learn and Understand AngularJS" },
+                    { 6, 2, 0, "Description for NodeJS", 149f, 2, "Learn and Understand NodeJS" },
+                    { 7, 3, 0, "Description for Programming for Beginners", 45f, 1, "Programming for Complete Beginners" },
+                    { 8, 4, 0, "Description 16 Hour Course", 150f, 1, "A 16 Hour C# Course with Visual Studio 2013" },
+                    { 9, 4, 0, "Description Learn Javascript", 20f, 1, "Learn JavaScript Through Visual Studio 2013" }
+                });
+        }
+    }
+}
