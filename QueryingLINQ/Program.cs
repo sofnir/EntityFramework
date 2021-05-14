493d593483a249d10230cba6ebde35027c1e5c16
macie <macie@DESKTOP-GUK2RJ2>
2021-05-14 16:48:33 +0200

    Querying LINQ: QueryableExplained

--- QueryingLINQ/Program.cs
+++ QueryingLINQ/Program.cs
@@ -31,5 +31,24 @@ namespace QueryingLINQ
             foreach (var c in courses)
                 Console.WriteLine(c.Name);
         }
+
+        static private void QueryableExplained()
+        {
+            var context = new PlutoContext();
+
+            //using IQueryable - call sql select with where level as part of the method
+            IQueryable<Course> courses = context.Courses;
+            var filtered = courses.Where(c => c.Level == Course.CourseLevel.Beginner);
+
+            foreach (var course in filtered)
+                Console.WriteLine(course.Name);
+
+            //using IEnumerable - call sql select without where (get all objects)
+            IEnumerable<Course> coursesesIEnumerable = context.Courses;
+            var filteredIEnumerable = coursesesIEnumerable.Where(c => c.Level == Course.CourseLevel.Beginner); //call sql select with where level as part of the method 
+
+            foreach (var courseIEnumerable in filteredIEnumerable)
+                Console.WriteLine(courseIEnumerable.Name);
+        }
     }
 }
