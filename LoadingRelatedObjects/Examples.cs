676f3678290f43e0d234ca5b1936c0bc2847974e
macie <macie@DESKTOP-GUK2RJ2>
2021-05-14 18:02:11 +0200

    LoadingRelatedObjects: EagerLoading

--- LoadingRelatedObjects/Examples.cs
+++ LoadingRelatedObjects/Examples.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace LoadingRelatedObjects
 {
@@ -19,11 +20,17 @@ namespace LoadingRelatedObjects
             foreach (var tag in courses.Tags)
                 Console.WriteLine(tag.Name);
         }
-
         public void NPlusOneIssue()
         {
             //throws exception - not supported in ef core
             var courses = Context.Courses.ToList();
+
+            foreach (var course in courses)
+                Console.WriteLine("{0} by {1}", course.Name, course.Author.Name);
+        }
+        public void EagerLoading()
+        {
+            var courses = Context.Courses.Include(c => c.Author).ToList();
 
             foreach (var course in courses)
                 Console.WriteLine("{0} by {1}", course.Name, course.Author.Name);
--- LoadingRelatedObjects/Program.cs
+++ LoadingRelatedObjects/Program.cs
@@ -7,7 +7,7 @@ namespace LoadingRelatedObjects
         static void Main(string[] args)
         {
             Examples examples = new Examples();
-            examples.NPlusOneIssue();
+            examples.EagerLoading();
         }
     }
 }
