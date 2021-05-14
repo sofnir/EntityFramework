a4b981bfbfa1cd1f017227c8d43af046b7347053
macie <macie@DESKTOP-GUK2RJ2>
2021-05-14 16:55:57 +0200

    LoadingRelatedObjects: Init

---
+++ LoadingRelatedObjects/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoadingRelatedObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
