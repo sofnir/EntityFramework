7ee41620f88e5a03a15aead30960022a6120e3db
macie <macie@DESKTOP-GUK2RJ2>
2021-05-20 15:57:17 +0200

    RepositoryPattern: Init

---
+++ RepositoryPattern/Core/Domain/Course.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.Core.Domain
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }                
+        public CourseLevel Level { get; set; }
+        public float FullPrice { get; set; }
+        public Author Author { get; set; }
+        public int AuthorId { get; set; }
+        public IList<Tag> Tags { get; set; }
+        public int CoverId { get; set; }
+        public Cover Cover { get; set; }
+
+        public enum CourseLevel
+        {
+            Beginner = 1,
+            Intermediate = 2,
+            Advanced = 3
+        }
+
+        public bool IsBeginnerCourse
+        {
+            get { return Level == CourseLevel.Beginner; }
+        }
+    }
+}
---
+++ RepositoryPattern/Persistence/PlutoContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryPattern.Core.Domain;
+using RepositoryPattern.Persistence.EntityConfigurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.Persistence
+{
+    public class PlutoContext : DbContext
+    {        
+        public PlutoContext()
+        {
+        }
+
+        public PlutoContext(DbContextOptions<PlutoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Tag> Tags { get; set; }        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-GUK2RJ2;Database=Pluto_RepositoryPattern;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CourseConfiguration()); 
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
